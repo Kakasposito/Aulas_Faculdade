6678e19fe238ffa460671713351ec137e4e7f00f
Kakasposito <124844359+Kakasposito@users.noreply.github.com>
2024-08-20 21:20:12 -0300

    Criado usando o Colab

---
+++ aula2.py
@@ -0,0 +1,62 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "toc_visible": true,
+      "authorship_tag": "ABX9TyOMYS7/tKjGcHJP5ZUxzodd",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Kakasposito/Aulas_Faculdade/blob/master/aula2.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": null,
+      "metadata": {
+        "id": "6jLrHaNA_hK2"
+      },
+      "outputs": [],
+      "source": [
+        "x = 10\n",
+        "nome = 'Marcus'\n",
+        "nota = 8.5\n",
+        "fez_inscricao = True\n",
+        "print(x, nome, nota, fez_inscricao)\n",
+        "print(type(x))\n",
+        "print(type(nome))\n",
+        "print(type(nota))\n",
+        "print(type(fez_inscricao))"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "nome = input('Digite um nome: ')\n",
+        "print(f'O nome digitado é: {nome}!')"
+      ],
+      "metadata": {
+        "id": "AxTsmvxyE81r"
+      },
+      "execution_count": null,
+      "outputs": []
+    }
+  ]
+}
